6605775ac402f5dcbd46ec8e0c802e7c7a1ec0ec
Rahul Raju <Rahul@Nikita-Rajus-MacBook-Pro.local>
2019-10-16 00:38:45 +1300

    This is new file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var d = new Date();
+d.setFullYear(2020);
+document.getElementById("result").innerHTML = d;
+
+
+
+var d = new Date();
+d.setDate(15);
+document.getElementById("result2").innerHTML = d;
+
+
+var d = new Date();
+d.setHours(22);
+document.getElementById("result3").innerHTML = d;
+
+
+var d = new Date();
+document.getElementById("result4").innerHTML = d.getMilliseconds();
